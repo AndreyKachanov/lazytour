24dfa86e176d02f47e1840824363d3899b51f76d
Andrey Kachanov <andrey170749@yandex.ru>
2015-05-31 17:50:07 +0300

    Adding header

--- index.php
+++ index.php
@@ -1,9 +1,49 @@
-<!DOCTYPE HTML>
+<!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
-
-</body>
+	<head>
+		<title>Lazytour</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=uft-8">
+		<link rel="stylesheet" type="text/css" href="style.css">
+		
+	</head>
+	<body>
+		<div id="wrapper">
+			<header>
+				<div class="top_menu">
+					<div class="child_left">
+						Ваш город: <span id="dashed_border">Москва</span>
+					</div>
+					<div class="child_center">
+						<ul>
+							<li><a href="">Турагентам</a></li>
+							<li><a href="">Как это работает</a></li>
+							<li><a href="">Отзывы</a></li>
+						</ul>		
+					</div>
+					<div class="child_right">
+						<ul>
+							<li id="dashed_border"><a href="">Вход</a></li>
+							<li id="separator"></li>
+							<li><a href="">Регистрация</a></li>
+						</ul>
+					</div>
+					<div class="protrusion"></div>		
+				</div>				
+				<div class="lazytour">
+					<h1 class="logo">
+						<a href="#">Lazytour</a>
+					</h1>
+					<div class="recreation">
+						<p>Отдых начинается<br>с поиска отдыха</p>
+					</div>
+					<div class="left_shadow"></div>
+					<div class="absolute"></div>
+					<div class="claim">
+						<button type="button" name="" value="" class="button_claim">Оставить заявку</button>
+					</div>	
+				</div>
+			</header>
+			
+		</div>					
+	</body>
 </html>
